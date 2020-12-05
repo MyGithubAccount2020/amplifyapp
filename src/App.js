47faa82ba94cc4403a8a6abea585cb597a5f36b1
MyGithubAccount2020 <74135741+MyGithubAccount2020@users.noreply.github.com>
2020-12-05 16:39:56 +0300

    Update App.js
    rev2 is using changes done in github branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload(rev1)
+          Edit <code>src/App.js</code> and save to reload(rev2-by github branch)
         </p>
         <a
           className="App-link"
